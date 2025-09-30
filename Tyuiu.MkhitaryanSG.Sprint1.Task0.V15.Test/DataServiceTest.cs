3b647ced7ec62a2faa50a5dbde7405e0b11cb859
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-09-30 15:18:31 +0500

    Task0

---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint1.Task0.V15.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate();
+            Assert.AreEqual(24, res);
+        }
+    }
+}
