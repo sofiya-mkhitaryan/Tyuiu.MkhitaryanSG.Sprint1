5b896bf3e7d094fbec45bf9f32eebeeee095cec3
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-09-30 16:39:55 +0500

    Task2

---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task2.V15.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task2.V15.Lib
+{
+    public class DataService : ISprint1Task2V15
+    {
+        public int CalculateCubeVolume(int value)
+        {
+            return value * value * value;
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint1.Task2.V15.Lib;
+//CalculateCubeVolume
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateCubeVolume(x);
+            Assert.AreEqual(8, res);
+
+        }
+    }
+}
