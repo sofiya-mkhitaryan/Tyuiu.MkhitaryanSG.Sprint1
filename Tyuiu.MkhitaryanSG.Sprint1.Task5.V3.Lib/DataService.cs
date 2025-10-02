dbab6a29b29613962c8d69d4c85ab9912a1a9fd6
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-02 16:35:20 +0500

    Task5

---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task5.V3.Lib
+{
+    public class DataService : ISprint1Task5V3
+    {
+        public int Calculate(int k)
+        {
+            if (k <= 0)
+                return -1;
+
+            string s = k.ToString();
+            if (s.Length < 3)
+                return -1;
+
+            return int.Parse(s[s.Length - 3].ToString());
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MkhitaryanSG.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task5.V3.Test;
+
+    [TestClass]
+public class DataServiceTest
+{
+    private DataService _dataService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _dataService = new DataService();
+    }
+
+    [TestMethod]
+    public void Calculate_NumberWithMoreThan3Digits_ReturnsThirdDigitFromEnd()
+    {
+        int k = 130985;
+        int expected = 9;
+        int actual = _dataService.Calculate(k);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Calculate_NumberWithExactly3Digits_ReturnsThirdDigitFromEnd()
+    {
+        int k = 123;
+        int expected = 1;
+        int actual = _dataService.Calculate(k);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Calculate_NumberWithLessThan3Digits_ReturnsMinusOne()
+    {
+        int k = 45;
+        int expected = -1;
+        int actual = _dataService.Calculate(k);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void Calculate_NonPositiveNumber_ReturnsMinusOne()
+    {
+        int k0 = 0;
+        int kNegative = -123;
+        Assert.AreEqual(-1, _dataService.Calculate(k0));
+        Assert.AreEqual(-1, _dataService.Calculate(kNegative));
+    }
+}
+
