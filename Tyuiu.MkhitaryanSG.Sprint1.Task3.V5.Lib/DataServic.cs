610677a81c9c42319959483031644b7d41625884
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-10-04 08:08:15 +0500

    Task3

---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task3.V5.Lib/DataServic.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task3.V5.Lib
+{
+    public class DataService : ISprint1Task3V5
+    {
+        public double DistanceLength(double mapScale, double distanceBetweenPoints)
+        {
+            double distanceInKm = distanceBetweenPoints * mapScale;//реальное расстояние в км
+            return Math.Round(distanceInKm, 3);//округление до трех знаков после запятой
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint1.Task3.V5.Test/DataServiceTes.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint1.Task3.V5.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double distanceInCm = 3.5;
+            double mapScale = 120.0;
+            double expectedDistanceInKm = distanceInCm * mapScale; // 420.0
+            var res = ds.DistanceLength(mapScale, distanceInCm);
+            Assert.AreEqual(420, res);
+        }
+    }
+}
